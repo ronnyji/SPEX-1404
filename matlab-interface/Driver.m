f622763b419eddb09fa7398b0b3aa2dfe6c0a229
Pengyu Ji <30428989+ronnyji@users.noreply.github.com>
2018-10-07 00:17:34 -0400

    Add static methods related to generating driver's commands

--- matlab-interface/Driver.m
+++ matlab-interface/Driver.m
@@ -1,13 +1,20 @@
 classdef Driver
-    properties
+    properties (SetAccess = private)
         comPort
         baudRate
         currentPosition
         driver
     end
 
+    properties (Constant)
+        startMarker = '<'
+        endMarker = '>'
+        CAN = char(30)
+        ACK = char(6)
+    end
+
     methods
-        function obj = Driver(comPort, baudRate)
+        function obj = Driver(comPort,baudRate)
             if nargin > 0
                 % Need to check type
                 obj.comPort = comPort;
@@ -22,6 +29,39 @@ classdef Driver
         function createInterface(obj)
             obj.driver = serial(obj.comPort,'BaudRate',obj.baudRate);
             fopen(obj.driver);
+        end
+    end
+
+    methods (Static)
+        function command = getCommandSetRemote()
+            command = strcat(Driver.startMarker,Driver.CAN);
+            command = strcat(command,',r');
+            command = strcat(command,Driver.endMarker);
+        end
+
+        function command = getCommandVariableSpeed(reverse,numStep)
+            command = strcat(Driver.startMarker,Driver.CAN);
+            command = strcat(command,',a,');
+            if reverse
+                command = strcat(command,'1,');
+            else
+                command = strcat(command,'0,');
+            end
+            command = strcat(command,num2str(numStep));
+            command = strcat(command,Driver.endMarker);
+        end
+
+        function command = getCommandConstantSpeed(reverse,numStep,period)
+            command = strcat(Driver.startMarker,Driver.CAN);
+            command = strcat(command,',c,');
+            if reverse
+                command = strcat(command,'1,');
+            else
+                command = strcat(command,'0,');
+            end
+            command = strcat(command,num2str(numStep),',');
+            command = strcat(command,num2str(period));
+            command = strcat(command,Driver.endMarker);
         end
     end
 end
