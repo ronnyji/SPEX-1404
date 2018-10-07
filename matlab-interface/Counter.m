c8ba7acad1ff0b2ec55bb64cc4d66e637d8c6691
Pengyu Ji <30428989+ronnyji@users.noreply.github.com>
2018-10-07 00:15:48 -0400

    Add static methods related to checksums of counter's commands

--- matlab-interface/Counter.m
+++ matlab-interface/Counter.m
@@ -1,5 +1,5 @@
 classdef Counter
-    properties
+    properties (SetAccess = private)
         boardIndex
         primaryAddress
         integrationTime
@@ -7,7 +7,7 @@ classdef Counter
     end
 
     methods
-        function obj = Counter(boardIndex, primaryAddress)
+        function obj = Counter(boardIndex,primaryAddress)
             if nargin > 0
                 % Need to check type
                 obj.boardIndex = boardIndex;
@@ -24,6 +24,27 @@ classdef Counter
             obj.counter.EOSCharCode = 10;
             obj.counter.EOSMode = 'write';
             fopen(obj.counter);
+        end
+    end
+
+    methods (Static)
+        function checksum = calculateChecksum(command)
+            checksum = sum(double(command));
+            checksum = mod(checksum,256);
+        end
+
+        function command = appendChecksum(command)
+            checksum = Counter.calculateChecksum(command);
+            command = strcat(command,num2str(checksum,'%03d'));
+        end
+
+        function valid = verifyChecksum(command)
+            checksum = Counter.calculateChecksum(command(1:end-3));
+            if checksum == str2double(command(end-2:end))
+                valid = true;
+            else
+                valid = false;
+            end
         end
     end
 end
